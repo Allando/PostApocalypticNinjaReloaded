48174a1a44976d35a71004f3f25a852d679e91a3
Allando W-Laptop <alla2634@edu.easj.dk>
2017-04-22 11:35:59 +0200

    stuff

--- PostApocalypticNinjaReloaded/Assets/Scripts/playerScript.cs
+++ PostApocalypticNinjaReloaded/Assets/Scripts/playerScript.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
-
 public class playerScript : MonoBehaviour
 {
-    public float Jumpforce;
-    public float MovingSpeed;
+    private float Jumpforce = 2.5f;
+    private float MovingSpeed = 0.2f;
     private Rigidbody2D rigidbody2D;
-    public float Speed = 10;
+    private Collision2D coll;
+    private float Speed = 10;
     public Physics2D Physics2D { get; set; }
 
     // Use this for initialization
@@ -18,30 +18,21 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
-        if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f) //LeftToUp
+        // DIRECTION = DOWN -> LEFT -> UP -> RIGHT
+        if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
         {
-            Debug.Log("Gravity Up");
-            Physics2D.gravity = new Vector3(0f, 10f, 0f); //UP
-
-            GravityUpControls();
-        }
-        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f) //DownToLeft
-        {
-            Debug.Log("Gravity Left");
-            Physics2D.gravity = new Vector3(-10f, 0f, 0f); //LEFT
-            GravityLeftControls();
-        }
-        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f) //RightToDown
-        {
-            Debug.Log("Gravity Down");
-            Physics2D.gravity = new Vector3(0f, -10f, 0f); //DOWN
             GravityDownControls();
         }
-        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f) //UpToRight
+        else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
         {
-            Debug.Log("Gravity Right");
-            Physics2D.gravity = new Vector3(10f, 0f, 0f); //RIGHT
+            GravityLeftControls();
+        }
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
+        {
+            GravityUpControls();
+        }
+        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
+        {
             GravityRightControls();
         }
 
@@ -53,37 +44,54 @@ public class playerScript : MonoBehaviour
     }
 
     /// <summary>
-    ///     Shifts cameraangle and gravity 90 degrees to the left
+    /// Shifts gravity clockwise!!
+    /// DIRECTION = DOWN -> LEFT -> UP -> RIGHT
     /// </summary>
     private void ShiftGravity()
     {
         //Keeping camera .. just in case
         //Camera.main.transform.Rotate(new Vector3(0, 0, -90));
 
-        if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f) //LeftToUp
-        {
-            Debug.Log("Gravity Up");
-            Physics2D.gravity = new Vector3(0f, 10f, 0f); //UP
-          
-            GravityUpControls();
-        }
-        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f) //DownToLeft
-        {
-            Debug.Log("Gravity Left");
-            Physics2D.gravity = new Vector3(-10f, 0f, 0f); //LEFT
-            GravityLeftControls();
-        }
-        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f) //RightToDown
+        //if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
+        //{
+        //    Debug.Log("Gravity Down");
+        //    Physics2D.gravity = new Vector3(0f, -10f, 0f);
+        //}
+        //else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
+        //{
+        //    Debug.Log("Gravity Left");
+        //    Physics2D.gravity = new Vector3(-10f, 0f, 0f);
+        //}
+        //else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
+        //{
+        //    Debug.Log("Gravity Up");
+        //    Physics2D.gravity = new Vector3(0f, 10f, 0f);
+        //}
+        //else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
+        //{
+        //    Debug.Log("Gravity Right");
+        //    Physics2D.gravity = new Vector3(10f, 0f, 0f);
+        //}
+
+        if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
         {
             Debug.Log("Gravity Down");
-            Physics2D.gravity = new Vector3(0f, -10f, 0f); //DOWN
-            GravityDownControls();
+            Physics2D.gravity = new Vector3(0f, -10f, 0f);
         }
-        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f) //UpToRight
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
+        {
+            Debug.Log("Gravity Left");
+            Physics2D.gravity = new Vector3(10f, 0f, 0f);
+        }
+        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
+        {
+            Debug.Log("Gravity Up");
+            Physics2D.gravity = new Vector3(0f, 10f, 0f);
+        }
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
         {
             Debug.Log("Gravity Right");
-            Physics2D.gravity = new Vector3(10f, 0f, 0f); //RIGHT
-            GravityRightControls();
+            Physics2D.gravity = new Vector3(-10f, 0f, 0f);
         }
     }
 
@@ -98,13 +106,13 @@ public class playerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Moving Left");
-            gameObject.transform.position += new Vector3(-MovingSpeed, 0, 0);
+            rigidbody2D.gameObject.transform.position += new Vector3(-MovingSpeed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Moving Right");
-            gameObject.transform.position += new Vector3(MovingSpeed, 0, 0);
+            rigidbody2D.gameObject.transform.position += new Vector3(MovingSpeed, 0, 0);
         }
     }
 
@@ -155,19 +163,32 @@ public class playerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rigidbody2D.AddForce(transform.right * Jumpforce * 100);
+            rigidbody2D.AddForce(transform.right * -Jumpforce * 100);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             Debug.Log("Moving Up");
-            gameObject.transform.position += new Vector3(0, -MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, MovingSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("Moving Down");
-            gameObject.transform.position += new Vector3(0, MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, -MovingSpeed, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "Platform")
+        {
+            Debug.Log("Collision with platform");
+            Jumpforce = 0;
+        }
+        else
+        {
+            Jumpforce = 2.5f;
         }
     }
 }
