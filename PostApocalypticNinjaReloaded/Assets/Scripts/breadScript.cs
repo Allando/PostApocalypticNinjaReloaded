ab3ec0861f963919b940a3d4262a7b8908c85f6b
Allando <alla2634@edu.easj.dk>
2017-04-23 19:28:43 +0200

    asd

---
+++ PostApocalypticNinjaReloaded/Assets/Scripts/breadScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breadScript : MonoBehaviour
+{
+
+    public Transform head;
+    public Transform[] segments;
+    List<Vector3> breadcrumbs;
+
+    Vector3 prevHeadPosition;
+    float headDisplacement;
+    public float segmentSpacing; //set controls the spacing between the segments,which is always constant.
+
+    void Start()
+    {
+        //populate the first set of crumbs by the initial positions of the segments.
+        breadcrumbs = new List<Vector3>();
+        breadcrumbs.Add(head.position); //add head first, because that's where the segments will be going.
+        for (int i = 0; i < segments.Length - 1; i++) // we don't want to the last segment to have a crumb.
+            breadcrumbs.Add(segments[i].position);
+        prevHeadPosition = head.position;
+    }
+
+    void Update()
+    {
+
+        Vector3 headMovement = head.position - prevHeadPosition;
+        float headMovementMagnitude = headMovement.magnitude;
+        headDisplacement += headMovementMagnitude;
+
+        if (headDisplacement >= segmentSpacing)
+        {
+            breadcrumbs.RemoveAt(breadcrumbs.Count - 1); //remove the last breadcrumb
+            breadcrumbs.Insert(0, head.position); // add a new one where head is.
+            headDisplacement = 0;
+        }
+        if (headMovementMagnitude > 0)
+        {
+            Vector3 slerped;
+            slerped = Vector3.Slerp(breadcrumbs[0], head.position, headDisplacement / segmentSpacing);
+            //do the first segment outside the loop because it needs head.position instead of next breadcrumb for interpolation
+            if ((breadcrumbs[0] - segments[0].position).sqrMagnitude > 0.01)
+            {
+                slerped = Vector3.Slerp(breadcrumbs[0], head.position, headDisplacement / segmentSpacing);
+                segments[0].position += headMovementMagnitude * (slerped - segments[0].position).normalized;
+                segments[0].LookAt(slerped);
+            }
+            for (int i = 1; i < segments.Length; i++)
+            {
+                if ((breadcrumbs[i] - segments[i].position).sqrMagnitude > 0.01)
+                {
+                    slerped = Vector3.Slerp(breadcrumbs[i], breadcrumbs[i - 1], headDisplacement / segmentSpacing);
+                    segments[i].position += headMovementMagnitude * (slerped - segments[i].position).normalized;
+                    segments[i].LookAt(slerped);
+                }
+            }
+            prevHeadPosition = head.position;
+        }
+    }
+}
--- PostApocalypticNinjaReloaded/Assets/Scripts/playerScript.cs
+++ PostApocalypticNinjaReloaded/Assets/Scripts/playerScript.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 public class playerScript : MonoBehaviour
 {
-    private float Jumpforce = 2.5f;
-    private float MovingSpeed = 0.2f;
+    //PLAYER MOVEMENT VALUES
+    private float jumpforce = 5f;
+    private float movingSpeed = 12.5f;
+
     private Rigidbody2D rigidbody2D;
     private Collision2D coll;
-    private float Speed = 10;
-    public Physics2D Physics2D { get; set; }
-
+   
     // Use this for initialization
     private void Start()
     {
@@ -18,28 +18,33 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // DIRECTION = DOWN -> LEFT -> UP -> RIGHT
-        if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
+        if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f) //If Gravity is down
         {
             GravityDownControls();
         }
-        else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
+        else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f) //If Gravity is left
         {
             GravityLeftControls();
         }
-        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f) //If Gravity is up
         {
             GravityUpControls();
         }
-        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
+        else if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f) //If Gravity is right
         {
             GravityRightControls();
+        } 
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Shifting Gravity Left");
+            ShiftGravityLeft();
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.LeftControl))
         {
-            Debug.Log("Shifting Gravity");
-            ShiftGravity();
+            Debug.Log("Shifting Gravity Right");
+            ShiftGravityRight();
         }
     }
 
@@ -47,31 +52,10 @@ public class playerScript : MonoBehaviour
     /// Shifts gravity clockwise!!
     /// DIRECTION = DOWN -> LEFT -> UP -> RIGHT
     /// </summary>
-    private void ShiftGravity()
+    private void ShiftGravityLeft()
     {
-        //Keeping camera .. just in case
+        //Keeping camera thing... just in case!
         //Camera.main.transform.Rotate(new Vector3(0, 0, -90));
-
-        //if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
-        //{
-        //    Debug.Log("Gravity Down");
-        //    Physics2D.gravity = new Vector3(0f, -10f, 0f);
-        //}
-        //else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
-        //{
-        //    Debug.Log("Gravity Left");
-        //    Physics2D.gravity = new Vector3(-10f, 0f, 0f);
-        //}
-        //else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
-        //{
-        //    Debug.Log("Gravity Up");
-        //    Physics2D.gravity = new Vector3(0f, 10f, 0f);
-        //}
-        //else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
-        //{
-        //    Debug.Log("Gravity Right");
-        //    Physics2D.gravity = new Vector3(10f, 0f, 0f);
-        //}
 
         if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
         {
@@ -95,100 +79,144 @@ public class playerScript : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Shift gravity counter clockwise
+    /// Down -> Right -> Up -> Left
+    /// </summary>
+    private void ShiftGravityRight()
+    {
+        if (Physics2D.gravity.x == 10f && Physics2D.gravity.y == 0f)
+        {
+            Debug.Log("Gravity Down");
+            Physics2D.gravity = new Vector3(0f, -10f, 0f);
+        }
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == -10f)
+        {
+            Debug.Log("Gravity Left");
+            Physics2D.gravity = new Vector3(-10f, 0f, 0f);
+        }
+        else if (Physics2D.gravity.x == -10f && Physics2D.gravity.y == 0f)
+        {
+            Debug.Log("Gravity Up");
+            Physics2D.gravity = new Vector3(0f, 10f, 0f);
+        }
+        else if (Physics2D.gravity.x == 0f && Physics2D.gravity.y == 10f)
+        {
+            Debug.Log("Gravity Right");
+            Physics2D.gravity = new Vector3(10f, 0f, 0f);
+        }
+    }
+
+    /// <summary>
+    /// Conrols if Gravity is down
+    /// </summary>
     private void GravityDownControls()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rigidbody2D.AddForce(transform.up * Jumpforce * 100);
+            rigidbody2D.AddForce(transform.up * jumpforce * 100);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Moving Left");
-            rigidbody2D.gameObject.transform.position += new Vector3(-MovingSpeed, 0, 0);
+            rigidbody2D.gameObject.transform.position += new Vector3(-movingSpeed * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Moving Right");
-            rigidbody2D.gameObject.transform.position += new Vector3(MovingSpeed, 0, 0);
+            rigidbody2D.gameObject.transform.position += new Vector3(movingSpeed * Time.deltaTime, 0, 0);
         }
     }
 
+    /// <summary>
+    /// Conrols if Gravity is left
+    /// </summary>
     private void GravityLeftControls()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rigidbody2D.AddForce(transform.right * Jumpforce * 100);
+            rigidbody2D.AddForce(transform.right * jumpforce * 100);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             Debug.Log("Moving Up");
-            gameObject.transform.position += new Vector3(0, MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, movingSpeed * Time.deltaTime, 0);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("Moving Down");
-            gameObject.transform.position += new Vector3(0, -MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, -movingSpeed * Time.deltaTime, 0);
         }
     }
 
+    /// <summary>
+    /// Conrols if Gravity is up
+    /// </summary>
     private void GravityUpControls()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rigidbody2D.AddForce(transform.up * -Jumpforce * 100); //THIS MIGHT BEE WRONG!!!
+            rigidbody2D.AddForce(transform.up * -jumpforce * 100);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Moving Right");
-            gameObject.transform.position += new Vector3(-MovingSpeed, 0, 0);
+            gameObject.transform.position += new Vector3(-movingSpeed * Time.deltaTime, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Moving Left");
-            gameObject.transform.position += new Vector3(MovingSpeed, 0, 0);
+            gameObject.transform.position += new Vector3(movingSpeed * Time.deltaTime, 0, 0);
         }
     }
 
+    /// <summary>
+    /// Conrols if Gravity is right
+    /// </summary>
     private void GravityRightControls()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rigidbody2D.AddForce(transform.right * -Jumpforce * 100);
+            rigidbody2D.AddForce(transform.right * -jumpforce * 100);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             Debug.Log("Moving Up");
-            gameObject.transform.position += new Vector3(0, MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, movingSpeed * Time.deltaTime, 0);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("Moving Down");
-            gameObject.transform.position += new Vector3(0, -MovingSpeed, 0);
+            gameObject.transform.position += new Vector3(0, -movingSpeed * Time.deltaTime, 0);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag != "Platform")
-        {
-            Debug.Log("Collision with platform");
-            Jumpforce = 0;
-        }
-        else
-        {
-            Jumpforce = 2.5f;
-        }
-    }
+    /// <summary>
+    /// Prevention of double jumps
+    /// </summary>
+    /// <param name="coll"></param>
+    //void OnCollisionEnter2D(Collision2D coll)
+    //{
+    //    if (coll.gameObject.tag != "Platform")
+    //    {
+    //        Debug.Log("Collision with platform");
+    //        jumpforce = 0;
+    //    }
+    //    else
+    //    {
+    //        jumpforce = 2.5f;
+    //    }
+    //}
 }
